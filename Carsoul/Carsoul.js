143187edde8c19ab95f8f12d34c5c949ea3e7e02
Jatin Seta <jatin@example.com>
2025-09-19 12:27:01 +0530

    Carsoul

---
+++ Carsoul/Carsoul.js
@@ -0,0 +1,49 @@
+let data = [
+    {
+        imgurl : "./Img/Img 1.jpg"
+    },
+    {
+        imgurl : "./Img/img 2.jpg"
+    },
+    {
+        imgurl : "./Img/img 3.jpg"
+    },
+    {
+        imgurl : "./Img/img 4.jpg"
+    }
+]
+let img = document.createElement('img')
+let length = data.length
+let count = 0
+img.src = data[count].imgurl
+
+document.getElementById("Next").addEventListener("click",function (){
+    count++
+        img.src = data[count].imgurl
+        console.log(count);
+        if(count === 3){
+            count = -1
+        }
+})
+
+document.getElementById("Prv").addEventListener("click",function (){
+    
+   
+        
+        if(count>0){
+        count--
+        console.log(count);
+        img.src = data[count].imgurl
+        
+    }else {
+        count = length -1
+        count--
+        img.src = data[count].imgurl
+        console.log(count);
+        }
+    
+})
+    
+console.log(count);
+
+document.getElementById("Imges").appendChild(img)
